8745535d5496bbd1baddefed5f08de6b24a2ad3c
Jack Parsons <jcksnparsons@yahoo.com>
2020-01-27 13:38:51 -0600

    added new .js files and start of universal fetch statement

---
+++ src/scripts/universalFetch.js
@@ -0,0 +1,7 @@
+const APImanager = {
+    searchAPI(searchCriteria) {
+        const criteria = encodeURIComponent(`"${searchCriteria}`);
+        const url = apiBaseURL + `'thing like ${criteria}'`;
+        return fetch(url).then(resp => resp.json)
+    }
+}
