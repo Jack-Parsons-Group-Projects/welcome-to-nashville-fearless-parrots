32f9900ccb4b5c6cccab2e23a7e797f388dd762f
Jack Parsons <jcksnparsons@yahoo.com>
2020-01-28 14:15:55 -0600

    added blank slate functionality to search results

--- src/scripts/domManipulator.js
+++ src/scripts/domManipulator.js
@@ -1,17 +1,6 @@
-const parkSearchButton = document.querySelector("#parkSearch-btn")
-const artSearchButton = document.querySelector("#artSearch-btn")
 const restaurantSearchButton = document.querySelector("#restSearch-btn")
-const concertSearchButton = document.querySelector("#concertSearch-btn")
 
 const searchResults = document.querySelector("#results")
-
-parkSearchButton.addEventListener("click", event => {
-
-})
-
-artSearchButton.addEventListener("click", event => {
-
-})
 
 restaurantSearchButton.addEventListener("click", event => {
     const restaurantsSearchCriteria = document.querySelector("#rest-search-criteria").value
@@ -19,13 +8,11 @@ restaurantSearchButton.addEventListener("click", event => {
     .then(parsedRestaurants => {
         const allRestaurants = parsedRestaurants.restaurants
 
+        searchResults.innerHTML = " "
+
         allRestaurants.forEach(restaurant => {
             searchResults.innerHTML += restaurantResultMaker(restaurant.restaurant.name, restaurant.restaurant.location.locality)
         });
     })
-})
-
-concertSearchButton.addEventListener("click", event => {
-
 })
 
