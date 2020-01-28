23e3039b3e19397f69b4e4743fcaad5265828da2
Chris Cotton <christopher.cotton88@gmail.com>
2020-01-28 15:18:34 -0600

    updated concertsAPI

--- src/scripts/APIs/concertsAPI/data.js
+++ src/scripts/APIs/concertsAPI/data.js
@@ -23,42 +23,7 @@ const concertsAPI = (concert) => {
 
 
 
-const searchResultsDomManager = {
-    busStopFactory(busStop, index) {
-      console.log("busStopFactory");
-  
-      // We use the "index" to make unique ids for each bus stop
-      return `
-        <section id="busstop-${index}" class="bus-stop">
-          <div id="routename-${index}" class="bus-stop__route-name">
-            ${busStop.route_name}
-          </div>
-          <div id="stopname-${index}" class="bus-stop__stopname">
-            ${busStop.stopname}
-          </div>
-          <button id="favorite-${index}" class="bus-stop__favorite">
-            Favorite &#11088;
-          </button>
-        </section>
-      `;
-    },
-    renderSearchResults(searchResults) {
-  
-      console.log("renderSearchResults");
-  
-      favoriteEventManager.removeFavoriteEventListeners();
-  
-      const container = document.querySelector("#search-results");
-      container.innerHTML = "";
-  
-      for (let i=0; i<searchResults.length; i++) {
-        const busStop = searchResults[i];
-        container.innerHTML += this.busStopFactory(busStop, i);
-      }
-  
-      favoriteEventManager.addFavoriteEventListeners();
-    }
-  };
+
 
 
 
