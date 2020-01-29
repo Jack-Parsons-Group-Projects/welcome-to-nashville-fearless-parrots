1fc66a44c57eef70a72a69ef244256a26eb277e5
Chase Sullins <jcsullins222@yahoo.com>
2020-01-29 11:59:04 -0600

    fetching api data and event listener on search button working, now working on dom manager and html factory

--- src/scripts/APIs/publicArtAPI/data.js
+++ src/scripts/APIs/publicArtAPI/data.js
@@ -1,3 +1,56 @@
-//API for Public Arts in Nashville
+const artApiManager = {
+  searchPublicArt(searchCriteria) {
+    const artApiUrl = `https://data.nashville.gov/resource/eviu-nxp6.json?`;
+    const criteria = encodeURIComponent(`"%${searchCriteria}%"`);
+    const url = artApiUrl + `$where=description like ${criteria}`;
+    return fetch(url).then(resp => resp.json());
+  }
+};
 
-const NashvillePublicArtApiKey = ""
+
+//event listener on search button
+const artSearchEventManager = {
+  addArtSearchEventListener() {
+    console.log("addArtSearchEventListener");
+
+    const artSearchButton = document.getElementById("artSearch-btn");
+
+    artSearchButton.addEventListener("click", () => {
+      const input = document.getElementById("art-search-criteria");
+
+      const searchCriteria = input.value;
+      const searchResultPromise = artApiManager.searchPublicArt(searchCriteria);
+      searchResultPromise.then(searchResults => {
+          console.log(searchResults)
+        searchResultsDomManager.renderSearchResults(searchResults);
+      });
+    });
+  }
+};
+
+
+//Dom manager / html factory
+const searchResultsDomManager = {
+    publicArtFactory(artWork, index) {
+        console.log("publicArtFactory");
+
+        return `
+        <section id="artwork-${index}"
+        `;
+    },
+    renderSearchResults(searchResults) {
+        console.log("renderSearchResults");
+
+        const container = document.querySelector("#results");
+        container.innerHTML = "";
+
+        for (let i=0; i<searchResults.length; i++) {
+            const artWork = searchResults[i];
+            container.innerHTML += this.publicArtFactory(artWork, i);
+        }
+    }
+}
+
+
+
+artSearchEventManager.addArtSearchEventListener()
