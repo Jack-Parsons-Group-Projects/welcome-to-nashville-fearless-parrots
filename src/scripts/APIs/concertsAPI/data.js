891551e6272495d3d2cc78999345daab77a94b0b
Chris Cotton <christopher.cotton88@gmail.com>
2020-01-29 23:40:27 -0600

    updated styling and js

--- src/scripts/APIs/concertsAPI/data.js
+++ src/scripts/APIs/concertsAPI/data.js
@@ -1,22 +1,33 @@
-const concertsApiFetch = {
-    searchForConcerts: (searchConcert) => {
-        return fetch(`http://app.ticketmaster.com/discovery/v1/events.json?apikey=${concertsAPIKey}&keyword=${searchConcert}`)
-            .then(response => response.json())
+// API Code
 
-    }
+const concertsApiFetch = {
+  searchForConcerts: searchConcert => {
+    return fetch(
+      `http://app.ticketmaster.com/discovery/v1/events.json?apikey=${concertsAPIKey}&keyword=${searchConcert}`
+    ).then(response => response.json());
+  }
 };
 
-concertsApiFetch.searchForConcerts(`rock`)
-    .then(concertsFromAPI => {
-        console.log(concertsFromAPI);
-    });
+concertsApiFetch.searchForConcerts(`concerts`).then(concertsFromAPI => {
+  console.log(concertsFromAPI);
+});
 
-const concertsHTMLFactory = (concert) => {
-    return `
+
+
+
+
+// Render To DOM Function
+
+const concertsHTMLFactory = (concert, date, time) => {
+  return `
       <section id="search-box">
-        <div>Title: ${concert.name}</div>
+      <div class="concert-search-result">
+      <div id="concert-search-result">
+        <div id="title">Title: ${concert.name}</div>
         <div>Date: ${concert.date}</div>
         <div>Time: ${concert.time}</div>
+        <button class="concert-save-button">Save</button>
+        </div>
       </section>
     `;
 };
@@ -24,36 +35,26 @@ const concertsHTMLFactory = (concert) => {
 
 
 
-const concertHTML = (concertName, concertDate, concertTime) => {
-    return `
-      <div class="concert-search-result">
-      <h3>${concertName}</h3>
-      <p>${concertDate}</p>
-      <p>${concertTime}</p>
-      <button class="concert-save-button">Save button</button>
-      </div>
-      `;
-  };
-  
 
+// Search API Event Feature
 
-
-
-const concertSearchButton = document.querySelector("#concertSearch-btn")
-// const concertSaveButton = document.querySelector(".concertSave-button")
+const concertSearchButton = document.querySelector("#concertSearch-btn");
 
 concertSearchButton.addEventListener("click", event => {
-    const concertSearchCriteria = document.querySelector("#concert-search-criteria").value
-    const concertSearchResults = document.querySelector("#results")
-    concertsApiFetch.searchForConcerts(concertSearchCriteria)
-        .then(concertsAPI => {
-            const concerts = concertsAPI._embedded.events
-            concertSearchResults.innerHTML = " "
-            concerts.forEach(concert => {
-                concertSearchResults.innerHTML += concertsHTMLFactory(concert)
-            });
-        })
-})
+  const concertSearchCriteria = document.querySelector(
+    "#concert-search-criteria"
+  ).value;
+  const concertSearchResults = document.querySelector("#results");
+  concertsApiFetch
+    .searchForConcerts(concertSearchCriteria)
+    .then(concertsAPI => {
+      const concerts = concertsAPI._embedded.events;
+      concertSearchResults.innerHTML = " ";
+      concerts.forEach(concert => {
+        concertSearchResults.innerHTML += concertsHTMLFactory(concert);
+      });
+    });
+});
 
 // for (let i=0; i<concertSearchResults.length; i++) {
 //     const concert = concertSearchResults[i];
@@ -64,18 +65,32 @@ concertSearchButton.addEventListener("click", event => {
 
 
 
-const saveConcertEventHandler = (event) => {
-    const buttonId = event.target.id;
-    const index = buttonId.split('-')[1];
+// Save To My Itinerary
 
-const 
+const concertSaveButton = document.querySelector(".concert-save-button");
 
+const concertSaveEventHandler = (event) => {
+  const buttonId = event.target.id;
+  const concert = buttonId.split("_")[1];
 
+  const concertsHTMLFactory = document.getElementById(`concert-search-result"-${concert}`");
+    concertSearchResults.innerHTML += concertsHTMLFactory(concert);
+};
 
-
-
-
-console.log(concertSearchButton)
+const saveConcertEventManager = {
+  addSaveEventListeners() {
+    const concertButtons = document.querySelectorAll(".concert-save-button");
+    for (let concertButton of concertButtons) {
+      concertButton.addEventListener("click", concertSaveEventHandler);
+    }
+  },
+  removeSaveEventListeners() {
+    const concertButtons = document.querySelectorAll(".concert-save-button");
+    for (let concertButton of concertButtons) {
+      concertButton.removeEventListener("click", favoriteEventHandler);
+    }
+  }
+};
 
 
 
